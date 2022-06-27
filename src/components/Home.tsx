d1f5fa940cf611b25378cf51c3b1c1230267d4ca
monikutee <petrulevicmonika@gmail.com>
2022-06-28 00:03:03 +0300

    unused check

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Link from "@mui/material/Link";
 import {
   Text,
   StyledRoot,
